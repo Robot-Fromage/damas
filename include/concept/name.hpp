c8a6d60717dddb0a659f1a9cf4c55a74b1161f42
Robot-Fromage <code@clementberthaud.com>
2021-04-18 13:58:12 +0200

    feat: add basis for files in concept category

---
+++ include/concept/name.hpp
@@ -0,0 +1,14 @@
+/*
+*   Damas
+*__________________
+* @file         damas.hpp
+* @author       Clement Berthaud
+* @brief        This file gathers include files for the Damas library.
+* @copyright    Copyright 2021 Clément Berthaud
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "core/core.hpp"
+#include "core/cpuinfo.hpp"
+#include "core/libinfo.hpp"
+
